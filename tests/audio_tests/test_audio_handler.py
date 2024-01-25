e11e2013c1816f7f199611594400b9475c5172c1
Georges Spyrides <georgesmss@gmail.com>
2024-01-24 22:52:01 -0300

    boilerplate for package

---
+++ tests/audio_tests/test_audio_handler.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestAudioHandler(unittest.TestCase):
+    def test_play_sound(self):
+        # Test implementation
+        pass
+
+# Similar structure for other test files
