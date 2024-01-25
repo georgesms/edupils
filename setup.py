27234848889ce79e0a5164762e4387fa11c2a7a5
Georges Spyrides <georgesmss@gmail.com>
2024-01-25 00:01:48 -0300

    without any requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='edupils',
-    version='0.1.1',
+    version='0.1.2',
     author='Georges Spyrides',
     author_email='georgesmss@gmail.com',
     packages=find_packages(),
